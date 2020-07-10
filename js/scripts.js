378dfcc73334fd63d548af8a89dbb9a3c3443d8c
cselleck <57884991+cselleck@users.noreply.github.com>
2020-07-10 02:17:40 -0500

    Adding what I have so far

--- js/scripts.js
+++ js/scripts.js
@@ -202,10 +202,119 @@
         }
     });
 
+    // Sliders on template.html
 
     /* Removes Long Focus On Buttons */
     $(".button, a, button").mouseup(function() {
         $(this).blur();
     });
+    var output1 = document.getElementById("demo1");
+    var slider1 = document.getElementById("ph");
+
+    output1.innerHTML = slider1.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider1.oninput = function() {
+        output1.innerHTML = this.value;
+    }
+
+    var slider2 = document.getElementById("fAcidity");
+    var output2 = document.getElementById("demo2");
+
+    output2.innerHTML = slider2.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider2.oninput = function() {
+        output2.innerHTML = this.value;
+    }
+    var output3 = document.getElementById("demo3");
+    var slider3 = document.getElementById("vAcidity");
+
+    output3.innerHTML = slider3.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider3.oninput = function() {
+        output3.innerHTML = this.value;
+    }
+
+    var output4 = document.getElementById("demo4");
+    var slider4 = document.getElementById("citric");
+
+    output4.innerHTML = slider4.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider4.oninput = function() {
+        output4.innerHTML = this.value;
+    }
+
+    var output5 = document.getElementById("demo5");
+    var slider5 = document.getElementById("rs");
+
+    output5.innerHTML = slider5.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider5.oninput = function() {
+        output5.innerHTML = this.value;
+    }
+
+    var output6 = document.getElementById("demo6");
+    var slider6 = document.getElementById("fsd");
+
+    output6.innerHTML = slider6.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider6.oninput = function() {
+        output6.innerHTML = this.value;
+    }
+
+    var output7 = document.getElementById("demo7");
+    var slider7 = document.getElementById("tsd");
+
+    output7.innerHTML = slider7.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider7.oninput = function() {
+        output7.innerHTML = this.value;
+    }
+
+    var output8 = document.getElementById("demo8");
+    var slider8 = document.getElementById("sulphates");
+
+    output8.innerHTML = slider8.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider8.oninput = function() {
+        output8.innerHTML = this.value;
+    }
+
+    var output9 = document.getElementById("demo9");
+    var slider9 = document.getElementById("chloride");
+
+    output9.innerHTML = slider9.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider9.oninput = function() {
+        output9.innerHTML = this.value;
+    }
+
+    var output10 = document.getElementById("demo10");
+    var slider10 = document.getElementById("density");
+
+    output10.innerHTML = slider10.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider10.oninput = function() {
+        output10.innerHTML = this.value;
+    }
+
+    var output11 = document.getElementById("demo11");
+    var slider11 = document.getElementById("alcohol");
+
+    output11.innerHTML = slider11.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider11.oninput = function() {
+        output11.innerHTML = this.value;
+    }
 
 })(jQuery);
