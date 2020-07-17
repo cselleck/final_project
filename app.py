a1b8846c80a2491635869e8ebcf14a5ce2adf6b3
cselleck <57884991+cselleck@users.noreply.github.com>
2020-07-17 00:12:31 -0500

    added explanation buttons to create page

--- app.py
+++ app.py
@@ -20,13 +20,14 @@ def get_delay():
         result=request.form.to_dict(flat=True)
         final_list=[[float(result['fixed_acidity']),float(result['volatile_acidity']),float(result['citric_acid']),float(result['residual_sugar']), float(result['chlorides']), float(result['free_sulphur_dioxide']), float(result['total_sulphur_dioxide']), float(result['density']), float(result['pH']), float(result['sulphates']), float(result['alcohol'])]]
         final_list=np.array(final_list)
-        print(final_list)
+        fa=float(result['fixed_acidity'])
+        va=float(result['volatile_acidity'])
         lg_from_joblib=joblib.load('static/data/finalized_model_joblib.obj')
         prediction = lg_from_joblib.predict(final_list)
         
         # print(prediction)
     
-        return render_template('template.html',prediction=prediction)
+        return render_template('template.html',prediction=prediction,fa=fa)
 
 
 @app.route("/")
